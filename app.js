630f93714ab0890449050e90bf88e9245608c134
thyholm1234 <christianhelligsoe@gmail.com>
2026-02-21 00:04:35 +0100

    v2.14

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ importCsvEl.addEventListener("change", async (event) => {
 
 exportCsvEl.addEventListener("click", () => {
   const rows = normalizeRows(state.rows, state.numDpu);
-  const headers = ["DPU", "Alder_år", "Alder_mdr", ...SCALE_NAMES];
+  const headers = ["DPU", "Alder (År)", "Alder (Mdr)", ...SCALE_NAMES];
   const lines = [headers.join(";")];
   rows.forEach((row) => {
     const cells = headers.map((h) => {
